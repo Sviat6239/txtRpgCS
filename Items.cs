3fc1fdb78937318973955c2472cbeece8bff8dd1
Sviat6239 <sviatoslav6239@gmail.com>
2025-12-05 19:55:21 +0100

    added some mechanics

---
+++ Items.cs
@@ -0,0 +1,6 @@
+class Items
+{
+    public Item Sword = new Item("Sword", 1, 500, 7, false, 0, 0, 5.0);
+    public Item HealthPotion = new Item("Health Potion", 10, 1, 0, true, 0, 20, 0.5);
+    public Item Bread = new Item("Bread", 10, 1, 0, true, 13, 5, 1);
+}
