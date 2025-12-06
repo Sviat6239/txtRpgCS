a27a3767bc78101f71a2ec8937eb95fac987095b
Sviat6239 <sviatoslav6239@gmail.com>
2025-12-06 18:51:39 +0100

    Added game loop

--- Program.cs
+++ Program.cs
@@ -7,9 +7,18 @@ class Program
     Player p = new Player();
 
 
+
+
     static void Main(string[] args)
     {
+        bool isRunning = true;
+
         Console.WriteLine("Welcome to simple text RPG!");
+
+        while (isRunning == true)
+        {
+
+        }
 
 
 
