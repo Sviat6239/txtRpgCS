604e82bea06c562cba7976e45720e920f6bf27bb
Sviat6239 <sviatoslav6239@gmail.com>
2025-12-07 16:01:26 +0100

    added alot of mobs names

--- src/MobName.cs
+++ src/MobName.cs
@@ -1,4 +1,410 @@
+using System.Collections.Generic;
+
 class MobNames
 {
+    List<string> GoblinNames = new List<string>()
+    {
+        "Goblin Gnaw",
+        "Goblin Sneak",
+        "Goblin Snapjaw",
+        "Goblin Slink",
+        "Goblin Cringer",
+        "Goblin Nibbler",
+        "Goblin Knucklebone",
+        "Goblin Flinger",
+        "Goblin Thorn",
+        "Goblin Murk"
+    };
+
+    List<string> OrcNames = new List<string>()
+    {
+        "Orc Bloodfang",
+        "Orc Skullcrusher",
+        "Orc Warbringer",
+        "Orc Ironhide",
+        "Orc Bonegnash",
+        "Orc Blacktooth",
+        "Orc Redaxe",
+        "Orc Grimjaw",
+        "Orc Stonefist",
+        "Orc Thunderfoot"
+    };
+
+    List<string> TrollNames = new List<string>()
+    {
+        "Troll Hillbreaker",
+        "Troll Stonecrusher",
+        "Troll Mossback",
+        "Troll Rockhide",
+        "Troll Clubfoot",
+        "Troll Bonehammer",
+        "Troll Fangtooth",
+        "Troll Mudbelly",
+        "Troll Ironjaw",
+        "Troll Thunderbelly"
+    };
+
+    List<string> SkeletonNames = new List<string>()
+    {
+        "Skeleton Rust",
+        "Skeleton Bonejaw",
+        "Skeleton Grim",
+        "Skeleton Shatter",
+        "Skeleton Fang",
+        "Skeleton Hollow",
+        "Skeleton Rattle",
+        "Skeleton Spire",
+        "Skeleton Creeper",
+        "Skeleton Wraith"
+    };
+
+    List<string> Zombie = new List<string>()
+    {
+        "Zombie Wanderer",
+        "Zombie Shambler",
+        "Zombie Groaner",
+        "Zombie Crawley",
+        "Zombie Moulder",
+        "Zombie Rotbelly",
+        "Zombie Lurcher",
+        "Zombie Snapper",
+        "Zombie Fang",
+        "Zombie Bleak"
+    };
+
+    List<string> DragonNames = new List<string>()
+    {
+        "Dragon Fireclaw",
+        "Dragon Ashwing",
+        "Dragon Emberheart",
+        "Dragon Flamefang",
+        "Dragon Skyflame",
+        "Dragon Ironwing",
+        "Dragon Stormtail",
+        "Dragon Frostbreath",
+        "Dragon Shadowwing",
+        "Dragon Blazeheart"
+    };
+
+    List<string> VampireNames = new List<string>()
+    {
+        "Vampire Nightshade",
+        "Vampire Bloodfang",
+        "Vampire Darkfang",
+        "Vampire Shadowfang",
+        "Vampire Crimson",
+        "Vampire Moonfang",
+        "Vampire Duskwalker",
+        "Vampire Nightstalker",
+        "Vampire Deathfang",
+        "Vampire Redclaw"
+    };
+
+    List<string> WraithNames = new List<string>()
+    {
+        "Wraith Shadow",
+        "Wraith Phantom",
+        "Wraith Hollow",
+        "Wraith Night",
+        "Wraith Fade",
+        "Wraith Whisper",
+        "Wraith Chill",
+        "Wraith Dusk",
+        "Wraith Lurk",
+        "Wraith Shade"
+    };
+
+    List<string> BansheeNames = new List<string>()
+    {
+        "Banshee Howl",
+        "Banshee Screech",
+        "Banshee Wail",
+        "Banshee Moan",
+        "Banshee Echo",
+        "Banshee Shriek",
+        "Banshee Night",
+        "Banshee Cry",
+        "Banshee Fade",
+        "Banshee Lament"
+    };
+
+    List<string> ImpNames = new List<string>()
+    {
+        "Imp Slyfoot",
+        "Imp Trickster",
+        "Imp Pester",
+        "Imp Snap",
+        "Imp Gremlin",
+        "Imp Mischief",
+        "Imp Nibble",
+        "Imp Fidget",
+        "Imp Scamp",
+        "Imp Gloom"
+    };
+
+    List<string> SlimeNames = new List<string>()
+    {
+        "Slime Ooze",
+        "Slime Glob",
+        "Slime Puddle",
+        "Slime Squish",
+        "Slime Blob",
+        "Slime Drip",
+        "Slime Mire",
+        "Slime Gloop",
+        "Slime Slush",
+        "Slime Squirm"
+    };
+
+    List<string> GolemNames = new List<string>()
+    {
+        "Golem Stone",
+        "Golem Iron",
+        "Golem Rock",
+        "Golem Clay",
+        "Golem Obsidian",
+        "Golem Crystal",
+        "Golem Bronze",
+        "Golem Granite",
+        "Golem Quartz",
+        "Golem Steel"
+    };
+
+    List<string> ElementalNames = new List<string>()
+    {
+        "Elemental Fire",
+        "Elemental Ice",
+        "Elemental Earth",
+        "Elemental Water",
+        "Elemental Air",
+        "Elemental Storm",
+        "Elemental Lava",
+        "Elemental Frost",
+        "Elemental Shadow",
+        "Elemental Light"
+    };
+
+    List<string> HarpyNames = new List<string>()
+    {
+        "Harpy Screech",
+        "Harpy Talon",
+        "Harpy Wind",
+        "Harpy Shadow",
+        "Harpy Night",
+        "Harpy Flame",
+        "Harpy Gale",
+        "Harpy Storm",
+        "Harpy Echo",
+        "Harpy Shriek"
+    };
+
+    List<string> MinotaurNames = new List<string>()
+    {
+        "Minotaur Horn",
+        "Minotaur Maze",
+        "Minotaur Brute",
+        "Minotaur Ironhide",
+        "Minotaur Stamp",
+        "Minotaur Club",
+        "Minotaur Fang",
+        "Minotaur Rage",
+        "Minotaur Bull",
+        "Minotaur Skull"
+    };
+
+    List<string> CyclopsNames = new List<string>()
+    {
+        "Cyclops Oneeye",
+        "Cyclops Boulder",
+        "Cyclops Smash",
+        "Cyclops Stone",
+        "Cyclops Club",
+        "Cyclops Fang",
+        "Cyclops Brute",
+        "Cyclops Roar",
+        "Cyclops Mud",
+        "Cyclops Thunder"
+    };
+
+    List<string> MermaidNames = new List<string>()
+    {
+        "Mermaid Tide",
+        "Mermaid Wave",
+        "Mermaid Coral",
+        "Mermaid Shell",
+        "Mermaid Pearl",
+        "Mermaid Song",
+        "Mermaid Reef",
+        "Mermaid Foam",
+        "Mermaid Dusk",
+        "Mermaid Dawn"
+    };
+
+    List<string> SirensNames = new List<string>()
+    {
+        "Sirens Call",
+        "Sirens Song",
+        "Sirens Wave",
+        "Sirens Tide",
+        "Sirens Lure",
+        "Sirens Echo",
+        "Sirens Whisper",
+        "Sirens Night",
+        "Sirens Sea",
+        "Sirens Mist"
+    };
+
+    List<string> PhoenixNames = new List<string>()
+    {
+        "Phoenix Flame",
+        "Phoenix Ash",
+        "Phoenix Ember",
+        "Phoenix Blaze",
+        "Phoenix Fire",
+        "Phoenix Cinder",
+        "Phoenix Wing",
+        "Phoenix Dawn",
+        "Phoenix Sky",
+        "Phoenix Heart"
+    };
+
+    List<string> GriffinNames = new List<string>()
+    {
+        "Griffin Talon",
+        "Griffin Wing",
+        "Griffin Beak",
+        "Griffin Sky",
+        "Griffin Storm",
+        "Griffin Roar",
+        "Griffin Claw",
+        "Griffin Flight",
+        "Griffin Thunder",
+        "Griffin Shadow"
+    };
+
+    List<string> HydraNames = new List<string>()
+    {
+        "Hydra Fang",
+        "Hydra Head",
+        "Hydra Venom",
+        "Hydra Scales",
+        "Hydra Tail",
+        "Hydra Spit",
+        "Hydra Maw",
+        "Hydra Poison",
+        "Hydra Snap",
+        "Hydra Rage"
+    };
+
+    List<string> LichNames = new List<string>()
+    {
+        "Lich Bone",
+        "Lich Skull",
+        "Lich Frost",
+        "Lich Dark",
+        "Lich Shadow",
+        "Lich Necro",
+        "Lich Curse",
+        "Lich Wraith",
+        "Lich Doom"
+    };
+
+    List<string> NecromancerNames = new List<string>()
+    {
+        "Necromancer Lord",
+        "Necromancer Shade",
+        "Necromancer Bone",
+        "Necromancer Skull",
+        "Necromancer Dark",
+        "Necromancer Shadow",
+        "Necromancer Curse",
+        "Necromancer Phantom",
+        "Necromancer Ghoul",
+        "Necromancer Rot"
+    };
+
+    List<string> DarkKnight = new List<string>()
+    {
+        "DarkKnight Black",
+        "DarkKnight Shadow",
+        "DarkKnight Steel",
+        "DarkKnight Fang",
+        "DarkKnight Doom",
+        "DarkKnight Ash",
+        "DarkKnight Iron",
+        "DarkKnight Skull",
+        "DarkKnight Night",
+        "DarkKnight Dread"
+    };
+
+    List<string> PaladinNames = new List<string>()
+    {
+        "Paladin Light",
+        "Paladin Dawn",
+        "Paladin Shield",
+        "Paladin Justice",
+        "Paladin Valor",
+        "Paladin Steel",
+        "Paladin Sun",
+        "Paladin Hope",
+        "Paladin Honor",
+        "Paladin Guardian"
+    };
+
+    List<string> RangerNames = new List<string>()
+    {
+        "Ranger Swift",
+        "Ranger Hawk",
+        "Ranger Arrow",
+        "Ranger Wind",
+        "Ranger Shadow",
+        "Ranger Hunter",
+        "Ranger Leaf",
+        "Ranger Trail",
+        "Ranger Forest",
+        "Ranger Tracker"
+    };
+
+    List<string> BerserNames = new List<string>()
+    {
+        "Assassin Blade",
+        "Assassin Shadow",
+        "Assassin Fang",
+        "Assassin Night",
+        "Assassin Dagger",
+        "Assassin Ghost",
+        "Assassin Silent",
+        "Assassin Poison",
+        "Assassin Venom",
+        "Assassin Strike"
+    };
+
+    List<string> BerserkerNames = new List<string>()
+    {
+        "Berserker Rage",
+        "Berserker Fang",
+        "Berserker Claw",
+        "Berserker Iron",
+        "Berserker Skull",
+        "Berserker Smash",
+        "Berserker Thunder",
+        "Berserker Blood",
+        "Berserker Brute",
+        "Berserker Fury"
+    };
+
+    List<string> Shaman = new List<string>()
+    {
+        "Shaman Spirit",
+        "Shaman Totem",
+        "Shaman Flame",
+        "Shaman Frost",
+        "Shaman Wind",
+        "Shaman Earth",
+        "Shaman Water",
+        "Shaman Lightning",
+        "Shaman Moon",
+        "Shaman Sun"
+    };
 
 }
