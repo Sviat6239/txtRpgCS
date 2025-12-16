1d6ab44c543ea04447b80cba5cc2e9111b06e04d
Sviat6239 <sviatoslav6239@gmail.com>
2025-12-16 03:18:01 +0100

    add basic biom configurator

---
+++ src/Bioms.cs
@@ -0,0 +1,12 @@
+using System.Threading.Channels;
+
+class Biom
+{
+    public string name;
+    public string description;
+    public string terreainType;
+    public double temperature;
+    public int mineCounts;
+    public int treeCounts;
+
+}
