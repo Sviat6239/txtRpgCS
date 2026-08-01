b388998dbabf94c52af8f00cb33e3fa6fe96a0f4
Sviat6239 <sviatoslav6239@gmail.com>
2026-08-01 18:27:25 +0200

    add properties and logic to Tile class for tile types and attributes

--- Src/Chunk.cs
+++ Src/Chunk.cs
@@ -2,5 +2,6 @@ namespace txtRPG.src.Chunk;
 
 public class Chunk
 {
-
+    public const int Weight = 512;
+    public const int Height = 512;
 }
--- Src/Tile.cs
+++ Src/Tile.cs
@@ -1,6 +1,61 @@
 namespace txtRPG.src.Tile;
 
+public enum TileType
+{
+    Air,
+    Grass,
+    Dirt,
+    Stone,
+    Coblestone,
+    Water,
+
+}
+
 public class Tile
 {
+    public TileType type { get; set; }
+    public int Hp { get; set; }
+    public int CordX { get; set; }
+    public int CordY { get; set; }
+    public bool IsSolid { get; set; }
+    public bool IsBreakable { get; set; }
 
+    public Tile(TileType type, int x, int y)
+    {
+        Type = type;
+        CordX = x;
+        CordY = y;
+
+        switch (type)
+        {
+            case TileType.Air:
+                Hp = 0;
+                IsSolid = false;
+                break;
+            case TileType.Grass:
+                Hp = 2;
+                IsSolid = false;
+                IsBreakable = false;
+                break;
+            case TileType.Dirt:
+                Hp = 2;
+                IsSolid = true;
+                IsBreakable = true;
+                break;
+            case TileType.Stone:
+                Hp = 5;
+                IsSolid = true;
+                IsBreakable = true;
+                break;
+            case TileType.Coblestone:
+                Hp = 4;
+                IsSolid = true;
+                IsBreakable = true;
+                break;
+            case TileType.Water:
+                Hp = 0;
+                IsSolid = false;
+                IsBreakable = false;
+        }
+    }
 }
